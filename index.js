d070aacbe698c9378106d3a64c09ca2e28eac635
Zachary Youngdale <zyoungdale@gmail.com>
2022-09-03 12:35:41 -0400

    commit tree 5e94884284b27553839cdca4362852cc54fe735c

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    const text = document.querySelector('#text')
+    text.textContent = "This is really cool!"
+  })
+
+
